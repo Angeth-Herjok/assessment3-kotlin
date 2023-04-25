51129906c2c31868dab8b683b2d91a00521e1d1b
Angeth-Herjok <angethherjok@gmail.com>
2023-04-25 08:34:25 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,106 @@
+fun main(args: Array<String>) {
+    people("mathew")
+    multiple()
+    validPassword("angethherjok@gmail.com")
+    var currentAccount=CurrentAccount(16247857,"Becky",5893.56)
+    println(currentAccount.balance++)
+    println(currentAccount.balance--)
+    currentAccount.details()
+}
+//1. Write and invoke one function that takes in a random string and returns the
+//following values: first character, last character, length of the string and
+//whether it begins with a vowel or not. (6pts)
+fun people(student:String){
+    var name="Mathew"
+   println(name[0])
+    println(name[5])
+    println(name.length)
+    var vowel="a,e,i,o,u"
+    if (name[0]==vowel[0]){
+        println(vowel)
+   }
+    else{
+        println("not")
+    }
+}
+//2. Create a function that takes in a password as a parameter. For a password to
+//be valid it must meet the following conditions:
+//1. Must be at least 8 characters long
+//2. Must be at most 16 characters long
+//3. Must not be “password”
+//4. Must contain a digit
+//Your function should determine whether the password provided is valid or not.
+//The function returns true/false (7pts)
+fun validPassword(password:String){
+    if (password=="8 characters"){
+        println("True")
+    }
+    else if (password=="16 characters"){
+        println("True")
+    }
+    else if (password!="password"){
+        println("True")
+    }
+    else if(password=="1"){
+        println("True")
+    }
+    else{
+        println("False")
+    }
+}
+//
+//3. Create a function that prints out multiples of 6 and 8 between 1 and 1000. For
+//multiples of both 6 and 8 it should print out “Bingo!” (5pts)
+fun multiple(){
+    for (nums in 1..1000){
+        if (nums % 6 ==0 && nums % 8==0){
+            println("Bingo!")
+        }
+        else{
+            println("Not among")
+        }
+    }
+}
+
+//4. Create a class CurrentAccount with the following attributes: account number,
+//account name, balance. It has the following functions:
+//a. deposit(amount: Double) - Increments the balance by the amount
+//deposited
+//b. withdraw(amount: Double) - Decrements the balance by the amount
+//withdrawn
+//c. details() - Prints out the account number and balance and name in
+//this format: “Account number x with balance y is operated by z” (6pts)
+class CurrentAccount(var accountNumber:Int,var accountName:String,var balance:Double){
+    fun deposit(amount:Double){
+       var amountDeposit=5900.809
+        balance++
+
+    }
+    fun withdraw(amount: Double){
+        var withdawaAmountl=6863.68
+        balance--
+
+    }
+    fun details(){
+        println("Account number $accountNumber with balance $balance is operated by $accountName")
+    }
+}
+
+//5. Create another class SavingsAccount. It has the same functions and
+//attributes as the current account except for one attribute, withdrawals: Int.
+//Withdrawals is a counter variable that is used to keep track of how many
+//withdrawals have been made from the account in a year. The only other
+//difference is that the savings account withdraw() method first checks if the
+//number of withdrawals is less than 4 for it to allow one to withdraw money
+//from the account. It also increments the withdrawals attribute after a
+//successful withdrawal (6pts)
+//
+class SavingsAccount(var accountNumber: Int,accountName: String,balance: Double){
+    fun withdrawal(withdrawals:Int){
+        if (withdrawals<=4){
+            println("Allow one to withdraw")
+        }
+    }
+}
+
+
